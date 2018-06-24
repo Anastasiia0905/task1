a7242e4761b90b5a3b0d3209ecab431c4854a79b
Anastasiia <anastasiia0905@gmail.com>
2018-06-24 20:24:23 +0300

    task-1

--- task-1/index.js
+++ task-1/index.js
@@ -6,10 +6,31 @@
  */
 function fillSpiralMatrix(n) {
   const result = [[]];
-
-  // Ваш код
-
-  return result;
+  let x = 0;
+  let y = 0;
+  let steps = n -1;
+  for(let i = 0; i < n; i++){result[i] = []};
+  for(let i = 1; i <= n*n; i++){
+    result[x][y] = i;
+    if(x === steps && y === n - steps - 1) steps--;
+    if((y >= x && y < steps) || (x - 1 === y && x === n - steps - 1)) y++;
+    else if(y <= x && y >= n - steps) y--;
+    else if(x <= y && x < steps) x++;
+    else if(x >= y && x >= n - steps) x--;
+  }
+  let row = '';
+  for(let i = 0; i < result.length; i++){
+    let cells = '';
+    for(let j = 0; j < result[i].length; j++) {
+      cells += '<th>' + result[i][j] + '</th>';
+    }
+    row += '<tr>' + cells + '</tr>';
+  }
+  document.querySelector('body').insertAdjacentHTML('beforeend', '<table>' + row + '</table');
+  const el = document.querySelectorAll('th');
+  for(let i = 0; i < el.length; i++){
+    el[i].style.cssText = "border: 1px solid black; width: 40px; height: 40px";
+  }
 }
 
 export default fillSpiralMatrix;
